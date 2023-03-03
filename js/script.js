fb0a0f1ef266e0211008194f7b5af679b872ee17
jarvis_codes <106377265+dotjarvis@users.noreply.github.com>
2023-03-03 12:35:44 +0300

    5th commit
    Making changes to the API for translation

--- js/script.js
+++ js/script.js
@@ -4,8 +4,6 @@ const toText = document.querySelector(".to-text");
 const translatebtn = document.querySelector("button");
 const exchangeIcon = document.querySelector(".fa-exchange-alt");
 const icons = document.querySelectorAll(".icons");
-const transBox = document.querySelector(".trans-box");
-const container = document.querySelector(".container");
 
 selectTag.forEach((tag, id) => {
   for (const country_code in countries) {
@@ -34,59 +32,13 @@ translatebtn.addEventListener("click", function (e) {
   if (!text) return;
   let translateFrom = selectTag[0].value;
   let translateTo = selectTag[1].value;
-  const apiurl = `https:api.mymemory.translated.net/get?q=${text}&langpair=${translateFrom}|${translateTo}`;
+  const apiurl = `https://api.mymemory.translated.net/get?q=${text}&langpair=${translateFrom}|${translateTo}`;
 
   fetch(apiurl)
-    .then((res) => {
-      responseClone = res.json();
-      return responseClone;
-    })
-    .then(
-      (data) => {
-        toText.value = data.responseData.translatedText;
-      },
-      function (rejectionReason) {
-        // 3
-        console.log(
-          "Error parsing JSON from response:",
-          rejectionReason,
-          responseClone
-        ); // 4
-        responseClone
-          .text() // 5
-          .then(function (bodyText) {
-            console.log(
-              "Received the following instead of valid JSON:",
-              bodyText
-            ); // 6
-          });
-      }
-    );
-
-  const after = `<br /><br />
-      <div class="word-by-word">Word by word</div><br />
-      <hr /><br />`;
-
-  // additional
-  const wordByWord = async function () {
-    const words = fromText.value.split(" ");
-    for (let word in words) {
-      const apiurl = `https:api.mymemory.translated.net/get?q=${words[word]}&langpair=${translateFrom}|${translateTo}`;
-
-      const res = await fetch(apiurl);
-      const data = await res.json();
-
-      const html = ` <textarea class="trans from" readonly>${words[word]}</textarea>
-        <textarea class="trans to" readonly>${data.responseData.translatedText}</textarea>`;
-
-      transBox.insertAdjacentHTML("beforeend", html);
-    }
-  };
-
-  transBox.innerHTML = "";
-  transBox.insertAdjacentHTML("beforeend", after);
-
-  wordByWord();
+    .then((res) => res.json())
+    .then((data) => {
+      toText.value = data.responseData.translatedText;
+    });
 });
 
 icons.forEach((icons) => {
@@ -104,6 +56,7 @@ icons.forEach((icons) => {
           utterance = new SpeechSynthesisUtterance(fromText.value);
           utterance.lang = selectTag[0].value;
         } else {
+          console.log(876543987653);
           utterance = new SpeechSynthesisUtterance(toText.value);
           utterance.lang = selectTag[1].value;
         }
